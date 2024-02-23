c01c0ba9b3e5083d0705cc2a3c5b3461b02ffed2
WIN-2QJDOS8CRCD\user <baxxromm@gmail.com>
2024-02-23 13:08:01 +0600

    added some logic

--- NewApp/User.cs
+++ NewApp/User.cs
@@ -1,7 +1,12 @@
-﻿namespace NewApp
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewApp
 {
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string? FullName { get; set; }
         public DateTime? BirthDate { get; set; }
